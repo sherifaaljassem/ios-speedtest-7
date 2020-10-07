4182148ddd32548b3c305f6371d7ea6f6907254a
sherifaaljassem <70379109+sherifaaljassem@users.noreply.github.com>
2020-10-07 17:04:36 +0300

    ok

---
+++ speedtest-7/speedtest/speedtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  speedtest
+//
+//  Created by Sherifa Aljassem on 10/7/20.
+//  Copyright © 2020 Sherifa Aljassem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
