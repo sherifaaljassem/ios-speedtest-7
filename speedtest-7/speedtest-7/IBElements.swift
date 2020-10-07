b3574a5099474b99a026cae06983753112a07cc2
Omar <omsi96@users.noreply.github.com>
2020-10-07 07:44:09 +0300

    ADD IBElements

---
+++ speedtest-7/speedtest-7/IBElements.swift
@@ -0,0 +1,248 @@
+/**
+ أوه! فتحت هالملف!
+لقد كشفت السر!
+ هذا الملف السحري، إن وضعته في أي مشروع، سيمكنك من استعمال البتنز الدائرية، مثل ما هو موضح في الستوري بورد الحالي
+طريقة هي، بعد أن تضع بتن بالستوري بورد، تقوم بتغيير الكلاس إلى `CustomButton`
+
+ مو بس `Buttons` دائرية، `textField`  و `UIVIew` دائري بعد
+ */
+import Foundation
+import UIKit
+
+@IBDesignable class CustomView: UIView {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable
+    var shadowOffset : CGSize{
+
+        get{
+            return layer.shadowOffset
+        }set{
+
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor : UIColor{
+        get{
+            return UIColor.init(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    @IBInspectable
+    var shadowOpacity : Float {
+
+        get{
+            return layer.shadowOpacity
+        }
+        set {
+
+            layer.shadowOpacity = newValue
+
+        }
+    }
+}
+
+
+
+@IBDesignable class CustomButton: UIButton {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable
+    var shadowOffset : CGSize{
+
+        get{
+            return layer.shadowOffset
+        }set{
+
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor : UIColor{
+        get{
+            return UIColor.init(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    @IBInspectable
+    var shadowOpacity : Float {
+
+        get{
+            return layer.shadowOpacity
+        }
+        set {
+
+            layer.shadowOpacity = newValue
+
+        }
+    }
+}
+
+
+
+
+
+
+@IBDesignable class CustomTextField: UITextField {
+
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            let color = UIColor.init(cgColor: layer.borderColor!)
+            return color
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    @IBInspectable
+    var shadowOffset : CGSize{
+
+        get{
+            return layer.shadowOffset
+        }set{
+
+            layer.shadowOffset = newValue
+        }
+    }
+
+    @IBInspectable
+    var shadowColor : UIColor{
+        get{
+            return UIColor.init(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    @IBInspectable
+    var shadowOpacity : Float {
+
+        get{
+            return layer.shadowOpacity
+        }
+        set {
+
+            layer.shadowOpacity = newValue
+
+        }
+    }
+}
